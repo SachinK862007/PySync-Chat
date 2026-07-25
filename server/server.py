9c8c4014c93bd5dc17ce533136b115b0294aa7a3
SachinK862007 <sk911862007@gmail.com>
2026-07-25 13:43:34 +0530

    basic Server.py id done

---
+++ server/server.py
@@ -0,0 +1,9 @@
+import asyncio
+
+async def start_server():
+    print("Starting PySync Chat Server...")
+    print("Preparing server configuration...")
+
+
+if __name__ == '__main__':
+    asyncio.run(start_server())
