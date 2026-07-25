1062f83bd5db52580574dda402f31eec6b9db9ef
SachinK862007 <sk911862007@gmail.com>
2026-07-25 15:04:57 +0530

    still working on the server.py

--- server/server.py
+++ server/server.py
@@ -1,8 +1,21 @@
 import asyncio
+from config import HOST, PORT
+
+async def handle_client(reader, writer):
+    pass
+
+
 
 async def start_server():
     print("Starting PySync Chat Server...")
     print("Preparing server configuration...")
+    print(f"HOST : {HOST}")
+    print(f"PORT : {PORT}")
+
+    server = await asyncio.start_server(handle_client, HOST, PORT)
+
+    print("Server created Successfully !")
+    
 
 
 if __name__ == '__main__':
