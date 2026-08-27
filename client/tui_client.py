258694265de31041829e33db78610a836636ef3a
Sachin Gowda K <sk911862007@gmail.com>
2026-08-27 20:08:44 +0530

    all the imports are done

--- client/tui_client.py
+++ client/tui_client.py
@@ -1,2 +1,17 @@
 import asyncio
 
+from textual.app import App, ComposeResult
+from textual.containers import Horizontal, Vertical, VerticalScroll, Container
+from textual.screen import ModalScreen
+from textual.widgets import (
+    Button,
+    Footer,
+    Header,
+    Input,
+    Label,
+    OptionList,
+    RichLog,
+    Static,
+)
+from textual.widgets.option_list import Option
+from server.config import HOST, PORT
